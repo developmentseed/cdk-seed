e83812442e4b3953996ad4592604569563624968
Mark Boyd <markdavidboyd@gmail.com>
2020-11-18 17:35:51 -0500

    update eslint configuraiton

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,26 +1,26 @@
 module.exports = {
+  env: {
+    'jest/globals': true,
+  },
   plugins: [
+    '@typescript-eslint',
     'jest',
   ],
   extends: [
     'airbnb-base',
+    'plugin:import/typescript',
     'plugin:@typescript-eslint/recommended',
   ],
-  env: {
-    'jest/globals': true,
-  },
-  overrides: [
-    {
-      files: ['*.ts'],
-      parser: '@typescript-eslint/parser',
-      plugins: [
-        '@typescript-eslint',
-      ],
-      parserOptions: {
-        project: './tsconfig.json',
-        tsconfigRootDir: __dirname,
-      },
-      extends: ['airbnb-typescript/lib/shared'],
+  parser: '@typescript-eslint/parser',
+  settings: {
+    'import/parsers': {
+      '@typescript-eslint/parser': ['.ts', '.tsx'],
     },
+  },
+  ignorePatterns: [
+    'packages/*/lib/*.js',
+    '*.d.ts',
+    'node_modules/',
+    '*.generated.ts',
   ],
 };
